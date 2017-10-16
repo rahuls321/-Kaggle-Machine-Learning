18986b5fd3c734cd9b917142a03ff6896932ffea
Rahul Kumar <29652757+rahuls321@users.noreply.github.com>
2017-10-16 13:19:45 +0530

    Random Forest Analysis

---
+++ Kaggle/Titanic ML From Disaster/Random Forest Classifier.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 12:14:28 2017
+
+@author: hp
+"""
+"""11. A Random Forest analysis in Python"""
+# Import the `RandomForestClassifier`
+from sklearn.ensemble import RandomForestClassifier
+
+# We want the Pclass, Age, Sex, Fare,SibSp, Parch, and Embarked variables
+features_forest = train[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
+
+# Building and fitting my_forest
+forest = RandomForestClassifier(max_depth = 10, min_samples_split=2, n_estimators = 100, random_state = 1)
+my_forest = forest.fit(features_forest, target)
+
+# Print the score of the fitted random forest
+print(my_forest.score(features_forest, target))
+
+# Compute predictions on our test set features then print the length of the prediction vector
+test_features = test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
+pred_forest = my_forest.predict(test_features)
+print(len(pred_forest))
